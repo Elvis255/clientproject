4039e35c7772e1ed92c28e13993456dc75c007b4
Elvis Maliza <emaliza2482@gmail.com>
2019-05-30 02:21:11 +0000

    Last Last Changes

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function displayResult(){
 }
 
 function placement(){
-    if (totalScore > 6 && totalScore <= 10) {
+    if (totalScore > 5 && totalScore <= 10) {
         playlist = "Chill";
         //youtubeLink= str.link("https://www.youtube.com/playlist?list=PL3tcXMQSY-kpOI48iTBIBtmcK3vPwDP4c")
     }else if(totalScore > 10 && totalScore <= 15){
@@ -23,20 +23,20 @@ function placement(){
         playlist = "Moody";
        //youtubeLink= str.link("https://www.youtube.com/playlist?list=PL3tcXMQSY-krzvf24poGX8ecl1YxAoSQe")
     }else if(totalScore > 20 && totalScore <= 24){
-        playlist = "Favorites";
+        playlist = "Curators' Favorites";
         //youtubeLink= str.link("https://www.youtube.com/playlist?list=PL3tcXMQSY-krzBjeRzisnmFAv6zb_8U0b")
     }
 }
 
 function description(){
     if(playlist === "Chill"){
-        return $(".result").append("<h4>Your playlist is Chill! Perfect for when you want to relax!</h4>");
+        return $(".result").append("<h4>Perfect for when you want to relax!</h4> <br><iframe width='560' height='315' src='https://www.youtube.com/embed/videoseries?list=PL3tcXMQSY-kpOI48iTBIBtmcK3vPwDP4c' frameborder='0' allow='accelerometer'; autoplay='true'; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>");
     }else if(playlist === "Upbeat"){
-        return $(".result").append("<h4>Your playlist is Upbeat! Someone's happy today!</h4>");
+        return $(".result").append("<h4>Someone's happy today!</h4> <br><iframe width='560' height='315' src='https://www.youtube.com/embed/videoseries?list=PL3tcXMQSY-koiPlf5Us_mbtM_nP5nmw-O' frameborder='0' allow='accelerometer; autoplay='true'; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>");
     }else if(playlist === "Moody"){
-        return $(".result").append("<h4>Your playlist is Moody. I guess you're in your bag today.</h4>");
+        return $(".result").append("<h4>I guess you're feeling some type of way today.</h4><br><iframe width='560' height='315' src='https://www.youtube.com/embed/videoseries?list=PL3tcXMQSY-krzvf24poGX8ecl1YxAoSQe' frameborder='0' allow='accelerometer; autoplay='true'; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>");
     }else if(playlist === "Favorites"){
-        return $(".result").append("<h4>Your playlist is Curators' Favorites! Time to discover some new music.</h4>");
+        return $(".result").append("<h4>Time to discover some new music.</h4> <br><iframe width='560' height='315' src='https://www.youtube.com/embed/videoseries?list=PL3tcXMQSY-krzBjeRzisnmFAv6zb_8U0b' frameborder='0' allow='accelerometer; autoplay='true'; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>");
     }
 }
 
